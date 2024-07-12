b1d9d3465986bea8e601464ea8d5d0996b6dd190
Sophia Brooks <scross@finnacloud.com>
2024-07-12 18:20:49 +0300

    mrrp meow - add more functions for future use.

--- src/main/java/com/finnacloud/finnacli/utils/AuthenticationHandler.java
+++ src/main/java/com/finnacloud/finnacli/utils/AuthenticationHandler.java
@@ -11,4 +11,24 @@ public class AuthenticationHandler {
         // get text from file
         return (new BufferedReader(new FileReader(file))).readLine();
     }
+
+    public static Boolean saveToken(String token) throws Exception {
+        File file = new File(System.getProperty("user.home") + "/.finnacloud/.token");
+        if (!file.exists()) {
+            file.getParentFile().mkdirs();
+            file.createNewFile();
+        }
+        // write token to file
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+        writer.write(token);
+        writer.close();
+        return true;
+    }
+
+    public static void deleteToken() throws Exception {
+        File file = new File(System.getProperty("user.home") + "/.finnacloud/.token");
+        if (file.exists()) {
+            file.delete();
+        }
+    }
 }
