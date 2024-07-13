fc384d235f4c184aac727e9a2df368296e78466c
Sophia Brooks <scross@finnacloud.com>
2024-07-13 15:36:36 +0300

    meow!! >//<

--- src/main/java/com/finnacloud/finnacli/apis/api/Resources.java
+++ src/main/java/com/finnacloud/finnacli/apis/api/Resources.java
@@ -3,15 +3,66 @@ package com.finnacloud.finnacli.apis.api;
 import com.finnacloud.finnacli.Constants;
 import com.finnacloud.finnacli.apis.ApplicationStruct;
 import com.finnacloud.finnacli.utils.AuthenticationHandler;
+import com.finnacloud.finnacli.utils.CommandLineTable;
 import com.finnacloud.finnacli.utils.HttpRequestBuilder;
 import com.finnacloud.finnacli.utils.HttpsRequestBuilder;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Resources implements ApplicationStruct {
     public Resources() {}
 
-    public void getResources(Integer limit, Integer offset, HashMap<String, String> filters) throws Exception {
-        System.out.println(HttpRequestBuilder.get(Constants.API_URL + "/resources", "GET", null, Constants.authenticationHandler.getToken()));
+    public void getResources(Integer limit, Integer offset, HashMap<String, String> filters, Boolean fullNotes) throws Exception {
+
+        CommandLineTable st = new CommandLineTable();
+
+
+        st.setShowVerticalLines(true);
+
+
+        String resourcesJson = HttpRequestBuilder.get(Constants.API_URL + "/resources", "GET", null, Constants.authenticationHandler.getToken());
+        Gson gson = new Gson();
+        Type type = new TypeToken<HashMap<String, Object>>(){}.getType();
+        HashMap<String, Object> resources = gson.fromJson(resourcesJson, type);
+
+        int index = 0;
+        if ((Boolean) resources.get("success")) {
+            st.setHeaders("", "Resource Name", "Status", "Notes", "IPv4 Addresses", "IPv6 Addresses", "Created On");
+            Map<String, Object> resourcesList = (Map<String, Object>) resources.get("resources");
+            ArrayList sharedResourcesList = (ArrayList) resourcesList.get("shared");
+
+            for (Object item : sharedResourcesList) {
+                Map<String, Object> sharedResource = (Map<String, Object>) item;
+                List<String> notesList = (List<String>) sharedResource.get("notes");
+                String notes = String.join(" ", notesList);
+                if (notes.length() > 16 && !fullNotes) {
+                    notes = notes.substring(0, 16) + "...";
+                }
+                st.addRow(""+index, sharedResource.get("resourceName").toString(), sharedResource.get("status").toString(), notes, sharedResource.get("ipv4Addresses").toString(), sharedResource.get("ipv6Addresses").toString(), sharedResource.get("createdOn").toString());
+                index++;
+            }
+
+            ArrayList ownedResourcesList = (ArrayList) resourcesList.get("owned");
+            for (Object item : ownedResourcesList) {
+                Map<String, Object> ownedResource = (Map<String, Object>) item;
+                List<String> notesList = (List<String>) ownedResource.get("notes");
+                String notes = String.join(" ", notesList);
+                if (notes.length() > 16 && !fullNotes) {
+                    notes = notes.substring(0, 16) + "...";
+                }
+                st.addRow(""+index, ownedResource.get("resourceName").toString(), ownedResource.get("status").toString(), notes, ownedResource.get("ipv4Addresses").toString().replace("[", "").replace("]", ""), ownedResource.get("ipv6Addresses").toString().replace("[", "").replace("]", ""), ownedResource.get("createdOn").toString());
+                index++;
+            }
+
+            st.print();
+        } else {
+            System.out.println("An error occurred while fetching resources: " + resources.get("error"));
+        }
     }
 }
--- src/main/java/com/finnacloud/finnacli/commands/command/Get.java
+++ src/main/java/com/finnacloud/finnacli/commands/command/Get.java
@@ -6,50 +6,61 @@ import com.finnacloud.finnacli.apis.api.Resources;
 import com.finnacloud.finnacli.commands.CommandInterface;
 import com.finnacloud.finnacli.commands.CommandStruct;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 
 public class Get extends CommandStruct implements CommandInterface {
-    public static Applications applications = new Applications();
+    private static final Applications applications = new Applications();
+    private final HashMap<String, String> apis = new HashMap<>();
+    private String[] arguments = new String[0];
 
     public Get() {
         super("Get", "Get data from the FinnaCloud API.", "get " + applications.getApis().keySet());
         apis.put("resources", "getResources");
     }
 
-    // array of hashmap to void
-    private HashMap<String, String> apis = new HashMap<>(); // string, methodName
-
     @Override
     public void run(String[] args) {
-        if (args.length == 1) {
+        if (args.length < 2) {
             System.out.println("Please provide a resource type. Available resource types are: " + apis.keySet());
             return;
         }
-        if(!apis.containsKey(args[1].toLowerCase())) {
+
+        String resourceType = args[1].toLowerCase();
+        if (!apis.containsKey(resourceType)) {
             System.out.println("Invalid resource type. Available resource types are: " + apis.keySet());
             return;
         }
 
-        apis.forEach((key, value) -> {
-            if (args[1].equalsIgnoreCase(key)) {
-                try {
-                    this.getClass().getMethod(value).invoke(this);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+        String methodName = apis.get(resourceType);
+
+        if (args.length > 2) {
+            arguments = new String[args.length - 2];
+            System.arraycopy(args, 2, arguments, 0, args.length - 2);
+        }
+
+        try {
+            this.getClass().getMethod(methodName).invoke(this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    public void getResources() { // we will do some fuckery
+    public void getResources() {
         if (!Runtime.isSilent) {
             System.out.println("Getting resources...");
         }
+
         try {
+            StringBuilder argBuilder = new StringBuilder();
+            for (String arg : arguments) {
+                argBuilder.append(arg).append(" ");
+            }
+            System.out.print(!argBuilder.toString().replaceAll(" ", "").isEmpty() ? argBuilder.toString() + "\n" : "");
+            // TODO: Remove the line above. (i'll probably forget)
+
             Resources resources = (Resources) applications.getApis().get("resources").getDeclaredConstructor().newInstance();
-            resources.getResources(5, 0, null);
-        } catch (Exception e){
+            resources.getResources(null, null, null, true);
+        } catch (Exception e) {
             System.out.printf("An error occurred while making the request: %s%n", e.getMessage());
         }
     }
